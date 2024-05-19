2dd88be75185ba5bae17ba8f06ad7aae30dbe0f6
Domenico Pagano <dpaganoh@gmail.com>
2024-05-19 14:59:02 -0500

    consola

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,8 @@
+//alert('Hola mundo!');
+
+/*
+const nombre = prompt('Cuál es tu nombre?');
+document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript moderno.`
+*/
+
+const producto = 'Monitor 24 pulgadas';
