3fb76b5e5c553daadf340bb99d3842e849fbab3d
Domenico Pagano <dpaganoh@gmail.com>
2024-05-19 21:46:02 -0500

    intro done!

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -5,4 +5,35 @@ const nombre = prompt('Cuál es tu nombre?');
 document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript moderno.`
 */
 
-const producto = 'Monitor 24 pulgadas';
+/* const producto = 'Monitor 24 pulgadas'; */
+
+//Time-tester
+console.time('Time-test');
+
+console.warn('Eso no está permitido');
+console.warn('Eso no está permitido');
+console.warn('Eso no está permitido');
+console.warn('Eso no está permitido');
+console.warn('Eso no está permitido');
+console.warn('Eso no está permitido');
+console.warn('Eso no está permitido');
+console.warn('Eso no está permitido');
+console.warn('Eso no está permitido');
+
+console.timeEnd('Time-test');
+
+//onjeto 'persona'
+const persona = {
+    nombre: 'Domenico',
+    apellido: 'Pagano',
+    adn: 1986
+}
+console.log(persona);
+console.log('Nombre: ' + persona.nombre);
+console.log('Apellido: ' + persona.apellido);
+console.log('Año de nacimiento: ' + persona.adn);
+
+/* Hola Mundo */
+console.log('Hola');
+console.log('Mundo');
+console.log('OK');
