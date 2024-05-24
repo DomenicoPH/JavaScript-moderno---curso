8f039b707dad283fc57d5c291b05aeea4899e7f7
Domenico Pagano <dpaganoh@gmail.com>
2024-05-24 11:06:27 -0500

    booleans

---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,12 @@
+// undefined
+let numero;
+console.log(numero)
+//console.log(typeof numero)
+
+// null
+let numeroNulo = null;
+console.log(numeroNulo);
+//console.log(typeof numeroNulo)
+
+console.log(numero == numeroNulo);
+console.log(numero === numeroNulo);
