d9172eb765b75f5af7cee5119e6386173905e01e
Domenico Pagano <dpaganoh@gmail.com>
2024-05-24 11:28:19 -0500

    objetos

---
+++ 06-Booleans/js/01-app.js
@@ -0,0 +1,16 @@
+// booleans
+const boolean1 = true;
+console.log(boolean1)
+console.log(typeof boolean1)
+
+const boolean2 = false;
+console.log(boolean2)
+console.log(typeof boolean2)
+
+const boolean3 = 'true';
+console.log(boolean1 == boolean3);
+console.log(boolean1 === boolean3);
+
+const boolean4 = new Boolean(true);
+console.log(boolean4)
+console.log(typeof boolean4)
---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,13 @@
+// booleans
+/* const autenticado = true; */
+const autenticado = false;
+
+// if... else...
+if (autenticado){
+    console.log('Puedes entrar')
+} else {
+    console.log('No puedes entrar')
+}
+
+// operador ternario
+console.log(autenticado ? 'Puedes entrar' : 'No puedes entrar');
