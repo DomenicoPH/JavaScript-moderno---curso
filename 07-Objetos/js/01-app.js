a6346a82573b056bfc6fcbe6d220e448a8db5455
Domenico Pagano <dpaganoh@gmail.com>
2024-05-25 11:33:18 -0500

    objetos

---
+++ 07-Objetos/js/01-app.js
@@ -0,0 +1,21 @@
+// objetos
+
+const nombre = "Monitor 20 pulgadas";
+const precio = 300;
+const disponible = true;
+
+// object literal
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 300,
+    disponible: true
+}
+
+console.log(nombre)
+console.log(precio)
+console.log(disponible)
+
+console.log(producto)
+console.log(producto.nombre)
+console.log(producto.precio)
+console.log(producto.disponible)
