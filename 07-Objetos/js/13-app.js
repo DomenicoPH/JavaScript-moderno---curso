f926835734a92df82ffc3e07a7881a4235d75c33
Domenico Pagano <dpaganoh@gmail.com>
2024-05-26 19:17:04 -0500

    arrays

--- 07-Objetos/js/13-app.js
+++ 07-Objetos/js/13-app.js
@@ -5,3 +5,32 @@ const producto = {
     disponible: true,
 }
 
+// Object.keys()
+console.log(Object.keys(producto));
+const keys = Object.keys(producto);
+    console.log(keys[0])
+    console.log(keys[1])
+    console.log(keys[2])
+
+// Object.values()
+console.log(Object.values(producto));
+const values = Object.values(producto);
+    console.log(values[0])
+    console.log(values[1])
+    console.log(values[2])
+
+// Object.entries()
+console.log(Object.entries(producto));
+const entries = Object.entries(producto)
+
+    console.log(entries[0])
+        console.log(entries[0][0])
+        console.log(entries[0][1])
+
+    console.log(entries[1])
+        console.log(entries[1][0])
+        console.log(entries[1][1])
+
+    console.log(entries[2])
+        console.log(entries[2][0])
+        console.log(entries[2][1])
