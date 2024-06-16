a9bad12039d0ec3e30d3c214eb8a1f91701226dd
Domenico Pagano <dpaganoh@gmail.com>
2024-06-16 08:11:35 -0500

    eventos

--- 14-Eventos/js/07-scripts.js
+++ 14-Eventos/js/07-scripts.js
@@ -1,1 +1,24 @@
 // Event Bubbling (Delegation)
+const cardDiv = document.querySelector('.card');
+
+/*
+cardDiv.addEventListener('click', () => {
+    console.log('click en .card');
+})
+*/
+
+cardDiv.addEventListener('click', (e) => {
+    console.log(`click en ${e.target.classList.value}`);
+})
+
+cardDiv.addEventListener('click', (e) => {
+    if(e.target.classList.contains('titulo')){
+        console.log('click en titulo')
+    }
+    if(e.target.classList.contains('precio')){
+        console.log('click en precio')
+    }
+    if(e.target.classList.contains('card')){
+        console.log('click en card')
+    }
+})
