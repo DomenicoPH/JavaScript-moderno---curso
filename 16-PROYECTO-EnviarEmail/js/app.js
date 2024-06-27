c5aef8b23977c28d5d752fe504927332a7be236b
Domenico Pagano <dpaganoh@gmail.com>
2024-06-27 11:36:34 -0500

    email

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
     // Asignar eventos
     inputEmail.addEventListener('blur', validar);
@@ -12,19 +13,38 @@ document.addEventListener('DOMContentLoaded', function(){
 
     // Función validadora
     function validar(e){
-        //console.log(e.target.value)
+        
+        //console.log(e.target.parentElement)
+
         if(e.target.value.trim() === ''){
-            mostrarAlerta();
-        } else {
-            console.log('hay algo')
-        }
+            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+            return
+        };
+
+        limpiarAlerta(e.target.parentElement);
     }
 
     // Función alerta
-    function mostrarAlerta(){
+    function mostrarAlerta(mensaje, referencia){
+        
+        // Comprueba si ya existe una alerta
+        limpiarAlerta(referencia);
+
+        // Generar Alerta en HTML
         const error = document.createElement('P');
-        error.textContent = 'Hubo un error';
-        console.log(error);
+        error.textContent = mensaje;
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center', 'error')
+        
+        // Inyectar el error al formulario:
+        referencia.appendChild(error)
+    }
+
+    // Función limpiarAlerta
+    function limpiarAlerta(referencia){
+        const alerta = referencia.querySelector('.error');
+        if(alerta){
+            alerta.remove();
+        }
     }
     
 })
