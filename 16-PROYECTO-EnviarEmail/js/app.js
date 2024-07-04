2a03dfd297c27738744e7211703f6eb353c6403b
Domenico Pagano <dpaganoh@gmail.com>
2024-07-04 09:11:24 -0500

    finalizado

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -12,13 +12,46 @@ document.addEventListener('DOMContentLoaded', () => {
     const inputMensaje = document.querySelector('#mensaje'); 
     const formulario = document.querySelector('#formulario');
     const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector('#spinner')
 
     // Eventos
     inputEmail.addEventListener('input', validar);
     inputAsunto.addEventListener('input', validar);
     inputMensaje.addEventListener('input', validar);
+    formulario.addEventListener('submit', enviarEmail);
+    
+    btnReset.addEventListener('click', (e) => {
+        e.preventDefault();
+        resetFormulario();
+    })
 
     // Funciones
+    function enviarEmail(e){
+        e.preventDefault();
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
+
+        setTimeout(() => {
+            spinner.classList.remove('flex');
+            spinner.classList.add('hidden');
+            resetFormulario();
+            
+            // Crear alerta
+            const alertaExito = document.createElement('P');
+            alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-10', 'font-bold', 'text-sm', 'uppercase');
+            alertaExito.textContent = 'Mensaje enviado correctamente';
+
+            formulario.appendChild(alertaExito);
+
+            setTimeout(() => {
+                alertaExito.remove();
+            }, 3000);
+
+        }, 3000);
+    };
+
+
     function validar(e){
 
         if(e.target.value.trim() === ''){
@@ -76,7 +109,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     function comprobarEmail(){
-        console.log(email);
+        //console.log(email);
         if(Object.values(email).includes('')){
             btnSubmit.classList.add('opacity-50');
             btnSubmit.disabled = true;
@@ -84,6 +117,17 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         btnSubmit.classList.remove('opacity-50');
         btnSubmit.disabled = false;
+    } // comprueba objeto 'email' y configura el boton SUBMIT
+
+
+    function resetFormulario(){
+        // -- Reset del objeto 'email' en js --
+        email.email = '';
+        email.asunto = '';
+        email.mensaje = '';
+        // -- Reset visual --
+        formulario.reset();
+        comprobarEmail();
     }
 
 });
