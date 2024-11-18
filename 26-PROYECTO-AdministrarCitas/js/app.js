3d3a4f4db036f05d2e9aab90869f8fa1f0596f81
Domenico Pagano <dpaganoh@gmail.com>
2024-11-18 08:13:01 -0500

    validaciones

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -1,9 +1,12 @@
-// Selectores
+/* Selectores */
+//inputs.........................................................
 const pacienteInput = document.querySelector('#mascota');
 const propietarioInput = document.querySelector('#propietario');
 const emailInput = document.querySelector('#email');
 const fechaInput = document.querySelector('#fecha');
 const sintomasInput = document.querySelector('#sintomas');
+//formulario.....................................................
+const formulario = document.querySelector('#formulario-cita');
 
 // Objeto de Cita
 const citaObj = {
@@ -16,14 +19,23 @@ const citaObj = {
 
 console.log(citaObj);
 
-// EVENTOS
+/* EVENTOS */
+// Eventos de los INPUTS................................
 pacienteInput.addEventListener('change', datosCita);
 propietarioInput.addEventListener('change', datosCita);
 emailInput.addEventListener('change', datosCita);
 fechaInput.addEventListener('change', datosCita);
 sintomasInput.addEventListener('change', datosCita);
+// Eventos del FORMULARIO...............................
+formulario.addEventListener('submit', submitCita);
 
+
+/* FUNCIONES */
 function datosCita(e){
     citaObj[e.target.name] = e.target.value;
-    console.log(citaObj);
+}
+
+function submitCita(e){
+    e.preventDefault();
+    console.log('submitiendo pe...')
 }
